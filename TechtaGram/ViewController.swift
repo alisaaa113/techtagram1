b4952c588eaa13394511057cd5fe3f8d76cedd34
arisa isshiki <alisa19970113@gmail.com>
2018-02-12 13:03:04 +0900

    Initial Commit

---
+++ TechtaGram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TechtaGram
+//
+//  Created by arisa isshiki on 2018/02/12.
+//  Copyright © 2018年 alisa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
